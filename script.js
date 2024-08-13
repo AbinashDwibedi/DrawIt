bd0eb80c990f8bbd73f420f5c58e14e9bd71c9ff
Abinash Dwibedi <133637759+AbinashDwibedi@users.noreply.github.com>
2024-08-13 10:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,33 +24,44 @@ canvas.addEventListener("touchend", end, false);
 canvas.addEventListener("mouseup", end, false);
 canvas.addEventListener("mouseout", end, false);
 
+function getCoordinates(event) {
+  if (event.type.includes("touch")) {
+    const touch = event.touches[0] || event.changedTouches[0];
+    return {
+      x: touch.clientX - canvas.offsetLeft,
+      y: touch.clientY - canvas.offsetTop
+    };
+  } else {
+    return {
+      x: event.clientX - canvas.offsetLeft,
+      y: event.clientY - canvas.offsetTop
+    };
+  }
+}
+
 function start(event) {
   is_drawing = true;
   context.beginPath();
-  context.moveTo(
-    event.clientX - canvas.offsetLeft,
-    event.clientY - canvas.offsetTop
-  );
-    event.preventDefault();
+  const { x, y } = getCoordinates(event);
+  context.moveTo(x, y);
+  event.preventDefault();
 }
-function selectColor(element) {
-  line_color = element.style.backgroundColor;
-}
+
 function draw(event) {
   if (is_drawing) {
     removedArray = [];
     removedArrayIndex = -1;
-    context.lineTo(
-      event.clientX - canvas.offsetLeft,
-      event.clientY - canvas.offsetTop
-    );
+    const { x, y } = getCoordinates(event);
+    context.lineTo(x, y);
     context.lineWidth = line_width;
     context.strokeStyle = line_color;
     context.lineCap = "round";
     context.lineJoin = "round";
     context.stroke();
+    event.preventDefault();
   }
 }
+
 function end(event) {
   if (is_drawing) {
     is_drawing = false;
@@ -62,6 +73,7 @@ function end(event) {
     index += 1;
   }
 }
+
 function undoLast() {
   if (index <= 0) {
     clearCanvas();
@@ -73,6 +85,7 @@ function undoLast() {
     context.putImageData(drawArray[index], 0, 0);
   }
 }
+
 function redoLast() {
   if (removedArrayIndex > -1) {
     drawArray.push(removedArray[removedArrayIndex]);
@@ -82,18 +95,22 @@ function redoLast() {
     context.putImageData(drawArray[index], 0, 0);
   }
 }
+
 function clearCanvas(element) {
   context.clearRect(0, 0, width, height);
   drawArray = [];
   index = -1;
 }
+
 function showHide() {
   document.getElementById("widthRange").classList.toggle("show");
 }
+
 function changeColor(element) {
   line_color = element.value;
   document.querySelector(".fa-droplet").style.color = element.value;
 }
+
 window.addEventListener("resize", () => {
   location.reload();
 });
